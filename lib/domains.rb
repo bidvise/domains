72c2f3e1ec71aab5209b8aa69208b410ec8ed2f7
Rene van Pelt <renevanpelt@Renes-MacBook-Pro-2.local>
2023-08-13 18:57:44 +0200

    initial commit

---
+++ lib/domains.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "domains/version"
+
+module Domains
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/domains/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Domains
+  VERSION = "0.1.0"
+end
